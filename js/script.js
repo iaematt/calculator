847d120b1a471f19831fd17c3ef782682e67fd84
Matheus Bastos <matheusbastos@outlook.com>
2020-11-11 16:22:36 -0300

    project running

--- js/script.js
+++ js/script.js
@@ -1,4 +1,37 @@
+var input = document.querySelector("#num");
+var operador;
 
-function clear() {
-  console.log('clear')
+function limpar() {
+  input.value = "";
+}
+
+function add_numero(num) {
+  input.value = input.value + num;
+}
+
+function add_operador(op) {
+  operador = op;
+  input.value = input.value + op;
+}
+
+function total() {
+  numeros = input.value.split(operador);
+
+  switch (operador) {
+    case "+":
+      input.value = Number(numeros[0]) + Number(numeros[1]);
+      break;
+
+    case "-":
+      input.value = numeros[0] - numeros[1];
+      break;
+
+    case "*":
+      input.value = numeros[0] * numeros[1];
+      break;
+
+    case "/":
+      input.value = numeros[0] / numeros[1];
+      break;
+  }
 }
